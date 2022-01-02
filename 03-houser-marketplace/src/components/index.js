fb820680f3a62d3ae383a0395296c5d1645ffc58
badawi1713 <dzaky.badawi@gmail.com>
2022-01-02 23:24:14 +0700

    Delete typo project

--- 03-houser-marketplace/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Navbar from "./Navbar";
-import PrivateRoutes from "./PrivateRoutes";
-import Spinner from "./Spinner";
-import OAuth from "./OAuth";
-import ListingItem from "./ListingItem";
-export { OAuth, ListingItem, Navbar, PrivateRoutes, Spinner };
